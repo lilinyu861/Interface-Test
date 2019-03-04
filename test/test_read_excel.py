d2630de1c5d66400d252f8243a75bcfebf1120ae
lily <lilinyu861@outlook.com>
2019-03-04 20:43:52 +0800

    修改测试代码

--- test/test_read_excel.py
+++ test/test_read_excel.py
@@ -1,5 +1,21 @@
 from common.read_excel import ReadExcel
+from common.write_to_excel import Writedata
 
-excel = ReadExcel("../data_origin/data_origin/test_read_excel.xlsx")
-s0, s1 = excel.read_and_save_cols_excel()
-print(s0, s1)
+
+class Test():
+    # 测试读取excel
+    def test_read_excel(self):
+        excel = ReadExcel("../test/data_origin/test_read_excel.xlsx")
+        s0, s1 = excel.read_and_save_cols_excel()
+        print(s0, s1)
+
+    # 测试向excel中写数据
+    def test_write_xcel(self):
+        s = [[1, 2], [0]]
+        W = Writedata()
+        W.data_write("../test/reports/test_write_excel.xls", s)
+
+
+if __name__ == '__main__':
+    Test().test_read_excel()
+    Test().test_write_xcel()
--- test/test_write_excel.py
+++
@@ -1,5 +0,0 @@
-from common.write_to_excel import Writedata
-
-s =[[1,2],[0]]
-W = Writedata()
-W.data_write("../test/reports/test_write_excel.xls", s)
